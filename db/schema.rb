27b2aa0df5b6d32b298e654000f210482b2142e5
Ho Viet Tuan <tuanhv258@gmail.com>
2017-01-11 22:11:55 +0700

    setup mysql2

--- db/schema.rb
+++ db/schema.rb
@@ -12,72 +12,72 @@
 
 ActiveRecord::Schema.define(version: 20161214164323) do
 
-  create_table "categories", force: :cascade do |t|
+  create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
-    t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.text     "description", limit: 65535
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
   end
 
-  create_table "category_types", force: :cascade do |t|
+  create_table "category_types", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "category_id", null: false
     t.integer  "type_id",     null: false
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.index ["category_id"], name: "index_category_types_on_category_id"
-    t.index ["type_id"], name: "index_category_types_on_type_id"
+    t.index ["category_id"], name: "index_category_types_on_category_id", using: :btree
+    t.index ["type_id"], name: "index_category_types_on_type_id", using: :btree
   end
 
-  create_table "companies", force: :cascade do |t|
+  create_table "companies", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
-    t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.text     "description", limit: 65535
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
   end
 
-  create_table "company_types", force: :cascade do |t|
+  create_table "company_types", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "company_id", null: false
     t.integer  "type_id",    null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["company_id"], name: "index_company_types_on_company_id"
-    t.index ["type_id"], name: "index_company_types_on_type_id"
+    t.index ["company_id"], name: "index_company_types_on_company_id", using: :btree
+    t.index ["type_id"], name: "index_company_types_on_type_id", using: :btree
   end
 
-  create_table "product_tags", force: :cascade do |t|
+  create_table "product_tags", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "product_id"
     t.integer  "tag_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["product_id"], name: "index_product_tags_on_product_id"
-    t.index ["tag_id"], name: "index_product_tags_on_tag_id"
+    t.index ["product_id"], name: "index_product_tags_on_product_id", using: :btree
+    t.index ["tag_id"], name: "index_product_tags_on_tag_id", using: :btree
   end
 
-  create_table "products", force: :cascade do |t|
+  create_table "products", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
     t.string   "code"
-    t.float    "price"
-    t.float    "sale_price"
-    t.float    "shipping_fee"
-    t.text     "description"
+    t.float    "price",        limit: 24
+    t.float    "sale_price",   limit: 24
+    t.float    "shipping_fee", limit: 24
+    t.text     "description",  limit: 65535
     t.string   "type"
     t.string   "size"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
-  create_table "tags", force: :cascade do |t|
+  create_table "tags", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
-    t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.text     "description", limit: 65535
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
   end
 
-  create_table "types", force: :cascade do |t|
+  create_table "types", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
-    t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.text     "description", limit: 65535
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
   end
 
 end
